7fd408e12dfc3e74c365df42d45c30975c0a3b3b
FeepingCreature <default_357-line@yahoo.de>
2016-11-21 08:31:55 +0100

    * implement new settings format (no more XML!) * remove block settings when block is removed from the world.

--- Data/Scripts/HoverRail/SettingsStore.cs
+++ Data/Scripts/HoverRail/SettingsStore.cs
@@ -2,15 +2,13 @@ using System;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
-using System.Xml.Serialization;
 using VRage.Serialization;
 using VRage.Utils;
 
 namespace HoverRail {
-	using StoreType = SerializableDictionary<string, string>;
 	static class SettingsStore {
 		const ushort HOVERRAIL_MESSAGE_ID = 59619; // see http://www.spaceengineerswiki.com/index.php?title=Registry_of_Message_Ids
-		const string KeyName = "HoverRailSettings";
+		const string KeyName = "HoverRailSettings_v2"; // format changed, from xml to cfg (good riddance)
 		
 		// network marker - message indicating a change of a setting to be shared
 		// format:
@@ -22,26 +20,42 @@ namespace HoverRail {
 		
 		const string REBROADCAST_MARKER = "HoverRail Rebroadcast"; // break loops
 		
-		static StoreType store;
+		static Dictionary<long, Dictionary<string, string>> dict;
 		static SettingsStore() {
-			store = new StoreType();
+			dict = new Dictionary<long, Dictionary<string, string>>();
 		}
 		static bool data_loaded = false;
 		public static T Get<T>(IMyEntity entity, string prop, T deflt) {
 			if (!data_loaded) Load();
-			var key = String.Format("{0}.{1}", entity.EntityId, prop);
-			var dict = store.Dictionary;
-			if (!dict.ContainsKey(key)) return deflt;
-			return (T)Convert.ChangeType(dict[key], typeof(T));
+			if (!dict.ContainsKey(entity.EntityId)) return deflt;
+			var dict2 = dict[entity.EntityId];
+			if (!dict2.ContainsKey(prop)) return deflt;
+			return (T)Convert.ChangeType(dict2[prop], typeof(T));
 		}
-		public static void SetKey(string key, string value, bool share = true) {
-			var dict = store.Dictionary;
-			if (!dict.ContainsKey(key)) dict.Add(key, value);
-			else dict[key] = value;
+		private static void SetKeyInternal(long id, string prop, string value) {
+			if (!dict.ContainsKey(id)) {
+				// first time seeing this entity
+				// set it up with our Close handler so we remove its settings when it gets removed
+				// (note: we can't use IMyEntity here because id might come from string parsing)
+				IMyEntity entity = null;
+				if (!MyAPIGateway.Entities.TryGetEntityById(id, out entity)) {
+					MyLog.Default.WriteLine(String.Format("WARNING: tried to set setting for unknown entity {0}, ignoring", id));
+					return;
+				}
+				dict.Add(id, new Dictionary<string, string>());
+				// MyLog.Default.WriteLine(String.Format("DEBUG: setting up OnClose for {0}", id));
+				entity.OnClose += HandleClose;
+			}
+			var dict2 = dict[id];
+			if (!dict2.ContainsKey(prop)) dict2.Add(prop, value);
+			else dict2[prop] = value;
+		}
+		public static void SetKey(long id, string prop, string value, bool share = true) {
+			SetKeyInternal(id, prop, value);
 			Save(); // euugh
 			if (share && !MyAPIGateway.Multiplayer.IsServer) {
-				var change_message = Encoding.UTF8.GetBytes(String.Format("{0}\n{1}={2}", CHANGE_MARKER, key, value));
-				MyLog.Default.WriteLine(String.Format("client> {0}={1}", key, value));
+				var change_message = Encoding.UTF8.GetBytes(String.Format("{0}\n{1}.{2}={3}", CHANGE_MARKER, id, prop, value));
+				MyLog.Default.WriteLine(String.Format("client> {0}={1}", prop, value));
 				MyAPIGateway.Multiplayer.SendMessageToServer(HOVERRAIL_MESSAGE_ID, change_message, true);
 			}
 		}
@@ -71,9 +85,13 @@ namespace HoverRail {
 				var str_message = Encoding.UTF8.GetString(data);
 				IEnumerable<string> changes = str_message.Split('\n').Skip(1);
 				foreach (var change in changes) {
+					// parse 12345.foo=True
 					var pair = change.Split(new char[] {'='}, 2);
 					string key = pair[0], value = pair[1];
-					SetKey(key, value, false);
+					var prop_id_pair = change.Split(new char[] {'.'}, 2);
+					string id_str = prop_id_pair[0], prop = prop_id_pair[1];
+					long id = Int64.Parse(id_str);
+					SetKey(id, prop, value, false);
 				}
 				if (MyAPIGateway.Multiplayer.IsServer) {
 					var rebroadcast_message = Encoding.UTF8.GetBytes(String.Format("{0}\n{1}", REBROADCAST_MARKER, str_message));
@@ -84,28 +102,75 @@ namespace HoverRail {
 			}
 		}
 		public static void Set(IMyEntity entity, string prop, object value) {
-			var key = String.Format("{0}.{1}", entity.EntityId, prop);
-			var dict = store.Dictionary;
 			var value_str = value.ToString();
-			SetKey(key, value_str);
+			SetKey(entity.EntityId, prop, value_str);
 		}
 		public static void Load() {
+			LoadOldSettings("HoverRailSettings");
 			MyLog.Default.WriteLine("HoverRail loading settings.");
 			data_loaded = true;
-			string value;
-			if (!MyAPIGateway.Utilities.GetVariable(KeyName, out value)) {
+			string serialized;
+			if (!MyAPIGateway.Utilities.GetVariable(KeyName, out serialized)) {
 				MyLog.Default.WriteLine("No settings found.");
 				return;
 			}
-			store = MyAPIGateway.Utilities.SerializeFromXML<StoreType>(value);
-			MyLog.Default.WriteLine("HoverRail done loading.");
+			IEnumerable<string> lines = serialized.Split('\n');
+			int num_ents = 0, num_settings = 0;
+			long current_id = -1;
+			foreach (string line in lines) {
+				if (line.StartsWith("[")) {
+					string id_str = line.Split('[', ']')[1];
+					current_id = Int64.Parse(id_str);
+					num_ents ++;
+				} else if (line.Contains("=")) {
+					if (current_id == -1) continue; // wrong order!
+					var pair = line.Split(new char[] {'='}, 2);
+					string prop = pair[0], value = pair[1];
+					SetKeyInternal(current_id, prop, value);
+					num_settings ++;
+				}
+			}
+			MyLog.Default.WriteLine(String.Format("HoverRail loaded {0} entities, {1} settings.", num_ents, num_settings));
+		}
+		public static void HandleClose(IMyEntity entity) {
+			MyLog.Default.WriteLine(String.Format("HoverRail entity {0} removed from world, deleting settings.", entity.EntityId));
+			dict.Remove(entity.EntityId);
+			Save();
 		}
 		public static void Save() {
 			MyLog.Default.WriteLine("HoverRail saving settings.");
-			string serialized = MyAPIGateway.Utilities.SerializeToXML(store);
-			// MyLog.Default.WriteLine(String.Format("debug: {0}", serialized));
+			var builder = new StringBuilder();
+			foreach (var entry in dict) {
+				builder.AppendFormat("[{0}]\n", entry.Key);
+				foreach (var entry2 in entry.Value) {
+					builder.AppendFormat("{0}={1}\n", entry2.Key, entry2.Value);
+				}
+			}
+			var serialized = builder.ToString();
+			// MyLog.Default.WriteLine(String.Format("DEBUG: {0}", serialized));
 			MyAPIGateway.Utilities.SetVariable(KeyName, serialized);
 			MyLog.Default.WriteLine("HoverRail done saving.");
+		}
+		// load legacy xml format settings
+		public static void LoadOldSettings(string old_key) {
+			string value;
+			if (!MyAPIGateway.Utilities.GetVariable(old_key, out value)) {
+				return;
+			}
+			var store = MyAPIGateway.Utilities.SerializeFromXML<SerializableDictionary<string, string>>(value);
+			int num_entries = 0;
+			foreach (var entry in store.Dictionary) {
+				string key = entry.Key;
+				var prop_id_pair = key.Split(new char[] {'.'}, 2);
+				string id_str = prop_id_pair[0], prop = prop_id_pair[1];
+				long id = Int64.Parse(id_str);
+				SetKeyInternal(id, prop, entry.Value);
+				num_entries ++;
+			}
+			Save();
+			MyLog.Default.WriteLine(String.Format("HoverRail converted {0} legacy entries.", num_entries));
+			// now we can safely clean up
+			MyAPIUtilities.Static.Variables.Remove(old_key);
 		}
 	}
 }
