1e61a27e16411b6fc5129c7a8333eae52155ced3
FeepingCreature <default_357-line@yahoo.de>
2016-11-26 16:48:29 +0100

    Add icons for actions, currently inoperable.

--- Mod/Textures/Icons/HoverRail/convert.bat
+++ Mod/Textures/Icons/HoverRail/convert.bat
@@ -1,43 +1,55 @@
-copy HoverRail_Straight_x10003.png HoverRail_Straight_x1.png
+copy HoverRail_Straight_x10000.png HoverRail_Straight_x1.png
 texconv HoverRail_Straight_x1.png
 del HoverRail_Straight_x1.png
 
-copy HoverRail_Straight_x30003.png HoverRail_Straight_x3.png
+copy HoverRail_Straight_x30000.png HoverRail_Straight_x3.png
 texconv HoverRail_Straight_x3.png
 del HoverRail_Straight_x3.png
 
-copy HoverRail_Straight_x100003.png HoverRail_Straight_x10.png
+copy HoverRail_Straight_x100000.png HoverRail_Straight_x10.png
 texconv HoverRail_Straight_x10.png
 del HoverRail_Straight_x10.png
 
-copy HoverRail_Straight_x300003.png HoverRail_Straight_x30.png
+copy HoverRail_Straight_x300000.png HoverRail_Straight_x30.png
 texconv HoverRail_Straight_x30.png
 del HoverRail_Straight_x30.png
 
-copy HoverRail_Curved_90_10x-12x0003.png HoverRail_Curved_90_10x-12x.png
+copy HoverRail_Curved_90_10x-12x0000.png HoverRail_Curved_90_10x-12x.png
 texconv HoverRail_Curved_90_10x-12x.png
 del HoverRail_Curved_90_10x-12x.png
 
-copy HoverRail_Engine0003.png HoverRail_Engine.png
+copy HoverRail_Engine0000.png HoverRail_Engine.png
 texconv HoverRail_Engine.png
 del HoverRail_Engine.png
 
-copy HoverRail_Slope_Top_x50003.png HoverRail_Slope_Top_x5.png
+copy HoverRail_Slope_Top_x50000.png HoverRail_Slope_Top_x5.png
 texconv HoverRail_Slope_Top_x5.png
 del HoverRail_Slope_Top_x5.png
 
-copy HoverRail_Slope_Bottom_x50003.png HoverRail_Slope_Bottom_x5.png
+copy HoverRail_Slope_Bottom_x50000.png HoverRail_Slope_Bottom_x5.png
 texconv HoverRail_Slope_Bottom_x5.png
 del HoverRail_Slope_Bottom_x5.png
 
-copy HoverRail_Slope_x50003.png HoverRail_Slope_x5.png
+copy HoverRail_Slope_x50000.png HoverRail_Slope_x5.png
 texconv HoverRail_Slope_x5.png
 del HoverRail_Slope_x5.png
 
-copy HoverRail_Junction_Left_10x-12x0003.png HoverRail_Junction_Left_10x-12x.png
+copy HoverRail_Junction_Left_10x-12x0000.png HoverRail_Junction_Left_10x-12x.png
 texconv HoverRail_Junction_Left_10x-12x.png
 del HoverRail_Junction_Left_10x-12x.png
 
-copy HoverRail_Junction_Right_10x-12x0003.png HoverRail_Junction_Right_10x-12x.png
+copy HoverRail_Junction_Left_10x-12x_Off0000.png HoverRail_Junction_Left_10x-12x_Off.png
+texconv HoverRail_Junction_Left_10x-12x_Off.png
+del HoverRail_Junction_Left_10x-12x_Off.png
+
+copy HoverRail_Junction_Left_10x-12x_On0000.png HoverRail_Junction_Left_10x-12x_On.png
+texconv HoverRail_Junction_Left_10x-12x_On.png
+del HoverRail_Junction_Left_10x-12x_On.png
+
+copy HoverRail_Junction_Left_10x-12x_OnOff0000.png HoverRail_Junction_Left_10x-12x_OnOff.png
+texconv HoverRail_Junction_Left_10x-12x_OnOff.png
+del HoverRail_Junction_Left_10x-12x_OnOff.png
+
+copy HoverRail_Junction_Right_10x-12x0000.png HoverRail_Junction_Right_10x-12x.png
 texconv HoverRail_Junction_Right_10x-12x.png
 del HoverRail_Junction_Right_10x-12x.png
